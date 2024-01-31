ed7d6e6ae5f59a88707b35ad8409ffe99b5733d3
Fred Wu <ifredwu@gmail.com>
2024-01-31 14:40:58 +1100

    Add support for `text-embedding-3`

--- lib/tiktoken_ruby.rb
+++ lib/tiktoken_ruby.rb
@@ -95,6 +95,8 @@ module Tiktoken
       "code-davinci-edit-001": "p50k_edit",
       # embeddings
       "text-embedding-ada-002": "cl100k_base",
+      "text-embedding-3-small": "cl100k_base",
+      "text-embedding-3-large": "cl100k_base",
       # old embeddings
       "text-similarity-davinci-001": "r50k_base",
       "text-similarity-curie-001": "r50k_base",
